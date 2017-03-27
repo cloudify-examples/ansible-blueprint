b5b7d15c790bd38410b18276fac0399e043d76ee
earthmant <earthmant@icloud.com>
2017-03-27 12:59:03 +0300

    updating for cloudify 4.0

--- scripts/aws-ansible/pre.sh
+++ scripts/aws-ansible/pre.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ctx source instance runtime-properties agentkey $(ctx target node properties cloudify_agent.key)
+ctx source instance runtime-properties agentkey $(ctx target node properties agent_config.key)
